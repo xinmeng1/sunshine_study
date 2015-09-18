c74cd8542cf7b56fa7f25007828c37550af5e4cf
Xin Meng <mengxin.city@gmail.com>
2015-09-18 22:47:17 +0100

    Lesson2: 1. HTTP请求获取数据,uri build URL 2. JSON数据解析 3. Adapter更新 4. 菜单操作,如何在Fragment添加菜单 5. AsyncTask的使用方法 6. Logcat的使用方法 7 权限

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.android.sunshine;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -38,7 +47,13 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
+//        if (id == R.id.refresh){
+//            FetchWeatherTask weatherTask = new FetchWeatherTask();
+//            weatherTask.execute("94043");
+//
+//        }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
